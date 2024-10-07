89a1e824e9f232b3965dd4bf9f4a8db1dff00a8e
mila811230 <bluegalaxy1230@gmail.com>
2024-10-07 12:43:02 +0900

    hello.html

--- src/main/java/com/mysite/sbb2/HelloController.java
+++ src/main/java/com/mysite/sbb2/HelloController.java
@@ -1,12 +1,16 @@
 package com.mysite.sbb2;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 public class HelloController {
 	
 	@GetMapping("/hello")
-	@ResponseBody
+//	@ResponseBody
 	public String hello() {
-		return "Hello World!!";
+		return "Hello";
 	}
 
 }
